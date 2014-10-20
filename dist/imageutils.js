b67d4169d60053d05403281cb3fb77a9cfc91a0a
Gregg Tavares <github@greggman.com>
2014-10-19 17:18:22 -0700

    update to tdl 0.0.8

--- dist/imageutils.js
+++ dist/imageutils.js
@@ -30,79 +30,10 @@
  */
 "use strict";
 
-define(function() {
-  /**
-   * Converts an RGB color value to HSV. Conversion formula
-   * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
-   * Assumes r, g, and b are contained in the set [0, 255] and
-   * returns h, s, and v in the set [0, 1].
-   *
-   * @param {number} r The red color value
-   * @param {number} g The green color value
-   * @param {number} b The blue color value
-   * @return {number[]} The HSV representation
-   */
-  var rgbToHsv = function(r, g, b) {
-    r = r / 255;
-    g = g / 255;
-    b = b / 255;
-    var max = Math.max(r, g, b);
-    var min = Math.min(r, g, b);
-    var h;
-    var s;
-    var v = max;
-
-    var d = max - min;
-    s = max == 0 ? 0 : d / max;
-
-    if (max == min) {
-      h = 0; // achromatic
-    } else {
-      switch(max) {
-        case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-        case g: h = (b - r) / d + 2; break;
-        case b: h = (r - g) / d + 4; break;
-      }
-      h /= 6;
-    }
-
-    return [h, s, v];
-  };
-
-  /**
-   * Converts an HSV color value to RGB. Conversion formula
-   * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
-   * Assumes h, s, and v are contained in the set [0, 1] and
-   * returns r, g, and b in the set [0, 255].
-   *
-   * @param {number} h The hue
-   * @param {number} s The saturation
-   * @param {number} v The value
-   * @return {number[]} The RGB representation
-   */
-  var hsvToRgb = function(h, s, v) {
-    var r;
-    var g;
-    var b;
-
-    var i = Math.floor(h * 6);
-    var f = h * 6 - i;
-    var p = v * (1 - s);
-    var q = v * (1 - f * s);
-    var t = v * (1 - (1 - f) * s);
-
-    switch(i % 6) {
-      case 0: r = v, g = t, b = p; break;
-      case 1: r = q, g = v, b = p; break;
-      case 2: r = p, g = v, b = t; break;
-      case 3: r = p, g = q, b = v; break;
-      case 4: r = t, g = p, b = v; break;
-      case 5: r = v, g = p, b = q; break;
-    }
-
-    return [r * 255 | 0, g * 255 | 0, b * 255 | 0];
-  };
-
+define([
+    './colorutils',
+  ], function(
+    colorUtils) {
   /**
    * Adjusts the hue, saturation and value of an image.
    * @param {HTMLImageElement|Image|HTMLCanvasElement} image an
@@ -168,14 +99,14 @@ define(function() {
     var numPixels = width * height;
     var numBytes = numPixels * 4;
     for (var offset = 0; offset < numBytes; offset += 4) {
-      var hsv = rgbToHsv(pixels[offset + 0], pixels[offset + 1], pixels[offset + 2]);
+      var hsv = colorUtils.rgb255ToHsv(pixels[offset + 0], pixels[offset + 1], pixels[offset + 2]);
       if (!validHue(hsv[0])) {
         continue;
       }
       var h = (hsv[0] + hue + 100) % 1;
       var s = Math.min(1, Math.max(0, hsv[1] + saturation));
       var v = Math.min(1, Math.max(0, hsv[2] + value));
-      var rgb = hsvToRgb(h, s, v);
+      var rgb = colorUtils.hsvToRgb255(h, s, v);
       pixels[offset + 0] = rgb[0];
       pixels[offset + 1] = rgb[1];
       pixels[offset + 2] = rgb[2];
@@ -331,9 +262,9 @@ define(function() {
   return {
     adjustHSV: adjustHSV,
     cropImage: cropImage,
-    hsvToRgb: hsvToRgb,
+    hsvToRgb: colorUtils.hsvToRgb255,
     makeTextImage: makeTextImage,
-    rgbToHsv: rgbToHsv,
+    rgbToHsv: colorUtils.rgb255ToHsv,
     scaleImage: scaleImage,
   };
 });
